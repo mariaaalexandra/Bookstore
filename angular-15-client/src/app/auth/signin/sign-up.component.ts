93f4c64ae2e28b1fdb59e06fc9b547f5f91f5f45
dariusbrinzan <brinzandarius27@gmail.com>
2023-04-26 00:31:22 +0300

    finished sign up

--- angular-15-client/src/app/auth/signin/sign-up.component.ts
+++ angular-15-client/src/app/auth/signin/sign-up.component.ts
@@ -1,102 +1,37 @@
 import { Component, OnInit,ViewEncapsulation } from '@angular/core';
 import { UserService } from "./../../services/user.service";
 import Keyboard from "simple-keyboard";
+import { HttpClient } from "@angular/common/http";
+import { FormsModule } from '@angular/forms';
+import { AppModule } from 'src/app/app.module';
 @Component({
   selector: 'app-sign-up',
   encapsulation: ViewEncapsulation.None,
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.css']
 })
-export class SignUpComponent implements OnInit {
-  userData: any;
-  keyboard: any;
-  value = "";
-  constructor(private user: UserService) { }
+export class SignUpComponent {
+  userData = {
+    username: '',
+    email: '',
+    password: ''
+  };
+  constructor(private http: HttpClient) { }
 
-  ngOnInit() {
-    this.user.currentUserData.subscribe((userData: any) => this.userData = userData)
-  }
-  signUp(data: any){
-    
-    this.user.changeData(data);
-  }
 
-  ngAfterViewInit() {
-    this.keyboard = new Keyboard({
-      onChange: input => this.onChange(input),
-      onKeyPress: button => this.onKeyPress(button),
-      mergeDisplay: true,
-      layoutName: "default",
-      layout: {
-        default: [
-          "q w e r t y u i o p",
-          "a s d f g h j k l",
-          "{shift} z x c v b n m {backspace}",
-          "{numbers} {space} {ent}"
-        ],
-        shift: [
-          "Q W E R T Y U I O P",
-          "A S D F G H J K L",
-          "{shift} Z X C V B N M {backspace}",
-          "{numbers} {space} {ent}"
-        ],
-        numbers: ["1 2 3", "4 5 6", "7 8 9", "{abc} 0 {backspace}"]
-      },
-      display: {
-        "{numbers}": "123",
-        "{ent}": "return",
-        "{escape}": "esc ⎋",
-        "{tab}": "tab ⇥",
-        "{backspace}": "⌫",
-        "{capslock}": "caps lock ⇪",
-        "{shift}": "⇧",
-        "{controlleft}": "ctrl ⌃",
-        "{controlright}": "ctrl ⌃",
-        "{altleft}": "alt ⌥",
-        "{altright}": "alt ⌥",
-        "{metaleft}": "cmd ⌘",
-        "{metaright}": "cmd ⌘",
-        "{abc}": "ABC"
-      }
-      
+  onSignUp(userData: { username: string, email: string, password: string }) {
+  console.log(userData);
+
+  this.http.post('http://localhost:8080/api/auth/signup', userData)
+    .subscribe((res) => {
+      console.log(res);
     });
-  }
 
-  onChange = (input: string) => {
-    this.value = input;
-    console.log("Input changed", input);
-  };
+ 
+}
 
-  onKeyPress = (button: string) => {
-    console.log("Button pressed", button);
+  
 
-    /**
-     * If you want to handle the shift and caps lock buttons
-     */
-    if (button === "{shift}" || button === "{lock}") this.handleShift();
-  };
-
-  onInputChange = (event: any) => {
-    this.keyboard.setInput(event.target.value);
-  };
-
-  handleShift = () => {
-    let currentLayout = this.keyboard.options.layoutName;
-    let shiftToggle = currentLayout === "default" ? "shift" : "default";
-
-    this.keyboard.setOptions({
-      layoutName: shiftToggle
-    });
-  };
-
-  handleNumbers() {
-    let currentLayout = this.keyboard.options.layoutName;
-    let numbersToggle = currentLayout !== "numbers" ? "numbers" : "default";
-
-    this.keyboard.setOptions({
-      layoutName: numbersToggle
-    });
-  }
-
+  
 
 }
